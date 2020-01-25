fbf7af65f7afd491509fb21ae224088524f56622
Jason M <jbird206@gmail.com>
2020-01-24 16:05:44 -0800

    Add files via upload
    bare minimum logic added to provide some results.

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,18 @@
-
-//User logic
-$(document).ready(function() {
-  //submit form
- $("#SubmitNum").submit(function(event) {
-     event.preventDefault();
-     input = parseInt($())
-
-  });
-});
+function myFunction() {
+  input = document.getElementById("myNumber").value;
+  myoutput = "";
+  for (i = 0; i <= input; i++) {
+    returnvalue = i.toString();
+    if (i.toString().includes("1")) {
+      returnvalue = "Beep";
+    };
+    if (i.toString().includes("2")) {
+      returnvalue = "Boop";
+    };
+    if (i.toString().includes("3")) {
+      returnvalue = "DAVE";
+    };
+    myoutput = myoutput.concat(returnvalue, "<li>");
+  }
+  document.getElementById("endResult").innerHTML = myoutput;
+}
